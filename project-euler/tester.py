d88e291619b9ab0e78ff5a8c33effd73c4480499
Janluke <gianlucacaronte@gmail.com>
2020-02-25 23:28:31 +0100

    add tester

---
+++ project-euler/tester.py
@@ -0,0 +1,35 @@
+#ghci pXX.hs -e 'res'
+import os
+import time
+import subprocess 
+def parse_solutions(file="solutions.md"):
+    sol = {}
+    def _parse(s): 
+        i = s.index('.')
+        k = int(s[:i])
+        sol[k] = s[i+1:].strip()
+    with open(file) as f: list(map(_parse, f.readlines()[4:]))
+
+    return sol
+
+
+solutions = parse_solutions()
+
+def do_test(sol_n,solutions = solutions,timeout=None):
+    if os.path.isfile("p{0:02d}.hs".format(sol_n)):
+        t = time.time()
+        cmd = ["ghci","p{0:02d}.hs".format(sol_n),"-e","res"]
+        out = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,timeout=timeout)\
+                .stdout.decode('utf-8').strip()
+
+        return time.time() - t, out==solutions[sol_n]
+    
+    return None,None
+
+for i in range(1,26):
+    try:
+        t, res = do_test(i,timeout=10)
+        if t != None :
+            print("{2:02d}:\t{0} [{1:.02f}s]".format("OK" if res else "KO", t,i))
+    except subprocess.TimeoutExpired:
+        print("{0:02d}:\tKO [TIMEOUT]".format(i))
