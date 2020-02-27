886be3fc99d95ae977443a16d56c1704c50ea735
Janluke <gianlucacaronte@gmail.com>
2020-02-27 19:24:55 +0100

    fix aspected solution misunderstanding of p9

--- project-euler/p009.hs
+++ project-euler/p009.hs
@@ -1,3 +1,12 @@
+{-
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+-}
 import Math.NumberTheory.Powers.Squares (integerSquareRoot)
 
 data Triplet = Triplet {a::Int, b::Int, c::Int} deriving(Show)
@@ -6,4 +15,6 @@ isPythagoreanTriplet (Triplet a b c) = ( a^2 + b^2 ) == c^2
 candidates =  filter isPythagoreanTriplet [ Triplet a b ( integerSquareRoot $ (a^2)+(b^2) )| a<- [1,2..1000], b<- [1,2..1000]]
 
 sumTriplet (Triplet a b c) = a+b+c
-res = head $ filter ((1000==).sumTriplet) candidates
+dotTriplet (Triplet a b c) = a*b*c
+
+res = dotTriplet $ head $ filter ((1000==).sumTriplet) candidates
