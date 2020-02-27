92d902f363c006a33af7cdf6f7d19e7f06a9bc87
Janluke <gianlucacaronte@gmail.com>
2020-02-27 19:43:52 +0100

    remove external dependency

--- project-euler/p009.hs
+++ project-euler/p009.hs
@@ -7,12 +7,14 @@ For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 -}
-import Math.NumberTheory.Powers.Squares (integerSquareRoot)
 
-data Triplet = Triplet {a::Int, b::Int, c::Int} deriving(Show)
+intSqrt :: Integer -> Integer
+intSqrt = truncate.sqrt.fromInteger
+
+data Triplet = Triplet {a::Integer, b::Integer, c::Integer} deriving(Show)
 isPythagoreanTriplet (Triplet a b c) = ( a^2 + b^2 ) == c^2
 
-candidates =  filter isPythagoreanTriplet [ Triplet a b ( integerSquareRoot $ (a^2)+(b^2) )| a<- [1,2..1000], b<- [1,2..1000]]
+candidates =  filter isPythagoreanTriplet [ Triplet a b ( intSqrt $ (a^2)+(b^2) )| a<- [1,2..1000], b<- [1,2..1000]]
 
 sumTriplet (Triplet a b c) = a+b+c
 dotTriplet (Triplet a b c) = a*b*c
